c42424054089b55a7d116f82260ce059eb3818d2
nour2403031-art <nour2403031@miuegypt.edu.eg>
2026-03-09 01:48:00 +0200

    Implement user signup and login validation
    Added signup and login validation functions to enhance user input checks.

--- js.js
+++ js.js
@@ -1,24 +1,82 @@
-window.addEventListener('scroll', function() {
+
+ window.addEventListener('scroll', function() {
     const hero = document.querySelector('.hero-container');
     const scrollPos = window.scrollY;
     const maxScroll = window.innerHeight;
 
-    // Calculate scroll progress (0 to 1)
     const scrollProgress = Math.min(scrollPos / maxScroll, 1);
 
-    // Zoom in as you scroll: starts at 100%, zooms to 180%
     let newSize = 100 + (scrollProgress * 80);
     
     hero.style.backgroundSize = newSize + '%';
     
-    // Smooth transition effect
     hero.style.transition = 'none';ero.style.backgroundSize = newSize + '%';
     
-    // Smooth transition effect
     hero.style.transition = 'background-size 0.1s ease-out';
  });
-//lastpage 
-window.addEventListener('scroll', () => {
+
+ function validatesignup() {
+    let Fname=document.getElementById('Fname').value;
+    let Fnameregex=/^[a-zA-Z\s]+$/;
+    if (!Fnameregex.test(Fname)) {
+        alert("First Name should be more than 3 characters, can only contain letters and spaces!");
+        return false;
+    }
+
+    let Lname=document.getElementById('Lname').value;
+    let Lnameregex=/^[a-zA-Z\s]+$/;
+    if (!Lnameregex.test(Lname)) {
+        alert("Last Name should be more than 3 characters, can only contain letters and spaces!");
+        return false;
+    }
+
+    let email=document.getElementById('Email').value;
+    let emailregex=/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+    if (!emailregex.test(email)) {
+        alert("Please enter a valid email address!");
+        return false;
+    }
+
+    let password=document.getElementById('Password').value;
+    let passwordregex=/^(?=.[A-Z])(?=.\d).{8,}$/;
+    if (!passwordregex.test(password)) {
+        alert("Password must be at least 8 characters long and include uppercase and atleast one number");
+        return false;
+    }
+
+    let confirmPassword=document.getElementById('Confirm_Password').value;
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return false;
+    }
+    alert("Account created successfully!");
+    return false;
+ }
+
+ function validateLogin(){
+
+    let email=document.getElementById('Login_Email').value;
+    let emailregex=/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+    if (!emailregex.test(email)) {
+        alert("Please enter a valid email address!");
+        return false;
+    }
+   
+        let password=document.getElementById('Login_Password').value;
+        if(password.length < 8) {
+            alert("Password must be at least 8 characters long!");
+            return false;
+        }
+        else if(password.length===0) {
+            alert("Please enter your password!");
+            return false;
+        }
+        alert("Logged in successfully!");
+    return false;
+ }
+
+
+ window.addEventListener('scroll', () => {
   const brand = document.querySelector('.footer-brand h1');
   const triggerBottom = window.innerHeight / 5 * 4;
   const brandTop = brand.getBoundingClientRect().top;
@@ -28,3 +86,4 @@ window.addEventListener('scroll', () => {
     brand.style.opacity = "1";
   }
 });
+ 
