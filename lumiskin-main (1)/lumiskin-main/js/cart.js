f664b6830bc492b35f560a2ef11f9ab2764427a6
hadeel2405026-sudo <hadeel2405026@miuegypt.edu.eg>
2026-04-20 05:03:29 +0200

    Fix formatting in cart.js

--- lumiskin-main (1)/lumiskin-main/js/cart.js
+++ lumiskin-main (1)/lumiskin-main/js/cart.js
@@ -1,4 +1,4 @@
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
+let cart = JSON.parse(localStorage.getItem('cart')) || []; 
 
 function saveCart() {
     localStorage.setItem('cart', JSON.stringify(cart));
