34c7dba72abdb73532c7e9a0ee549b22e74a23be
lina2405707-creator <lina2405707@miuegypt.edu.eg>
2026-05-25 14:45:05 -0700

    FIX: corrected adminController file path casing symmetry

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const adminController = require('../controllers/admincontroller');
+const adminController = require('../controllers/adminController');
 const { requireLogin } = require('../middleware/auth');
 
 // Admin page requires login
